c8ddac40b9eab9708b4ad3614982e22fd9acb094
Sky Wilshaw <zeramorphic@proton.me>
2025-03-06 12:52:51 +0000

    finInjToNominal
    Signed-off-by: Sky Wilshaw <zeramorphic@proton.me>

--- Nominal/Category/FinInjColimit.lean
+++ Nominal/Category/FinInjColimit.lean
@@ -6,6 +6,8 @@ import Nominal.Category.FinInj
 # Colimits of shape `FinInj`
 
 In this file, we construct the colimit in `Type` for a diagram of shape `FinInj`.
+Our main objective is to construct the functor
+`finInjToNominal : (FinInj 𝔸 ⥤ Type) ⥤ Bundled (Nominal 𝔸)`.
 -/
 
 open CategoryTheory Functor Limits
@@ -88,26 +90,23 @@ def finInjCocone_isColimit.{u, v} {𝔸 : Type u} [Infinite 𝔸] (F : FinInj �
 def finInjColimit.{u, v} (𝔸 : Type u) [Infinite 𝔸] :
     (FinInj 𝔸 ⥤ Type (max u v)) ⥤ Type (max u v) where
   obj := finInjColimitApex
-  map a := Quotient.lift (λ x ↦ Quotient.mk _ ⟨x.1, a.app x.1 x.2⟩) <| by
-    rintro ⟨s, x⟩ ⟨t, y⟩ ⟨u, f, g, h⟩
-    apply Quotient.sound
-    refine ⟨u, f, g, ?_⟩
-    simp only
-    refine (congr_arg (· x) (a.naturality f)).symm.trans ?_
-    rw [types_comp_apply, h]
-    exact (congr_arg (· y) (a.naturality g))
-  map_id β := by
-    ext x
-    induction x using Quotient.inductionOn
-    case h x =>
-    simp only [types_comp_apply, id_eq, eq_mpr_eq_cast, cast_eq, NatTrans.id_app, types_id_apply,
-      Sigma.eta, Quotient.lift_mk]
-  map_comp f g := by
-    ext x
-    induction x using Quotient.inductionOn
-    case h x =>
-    simp only [types_comp_apply, id_eq, eq_mpr_eq_cast, cast_eq, FunctorToTypes.comp,
-      Quotient.lift_mk]
+  map {F G} f := (finInjCocone_isColimit F).desc ((Cocones.precompose f).obj (finInjCocone G))
+  map_id F := by
+    dsimp only
+    rw [← (finInjCocone_isColimit F).uniq]
+    intro
+    rfl
+  map_comp {F G H} f g := by
+    dsimp only
+    rw [← (finInjCocone_isColimit F).uniq]
+    intro
+    rfl
+
+theorem finInjColimit_map.{u, v} (𝔸 : Type u) [Infinite 𝔸] {F G : FinInj 𝔸 ⥤ Type (max u v)}
+    (f : F ⟶ G) :
+    (finInjColimit 𝔸).map f =
+    (finInjCocone_isColimit F).desc ((Cocones.precompose f).obj (finInjCocone G)) :=
+  rfl
 
 def finInjLeg.{u, v} {𝔸 : Type u} [Infinite 𝔸] {F : FinInj 𝔸 ⥤ Type (max u v)}
     (s : FinInj 𝔸) (x : F.obj s) :
@@ -158,6 +157,9 @@ theorem finInjPermCocone_ι_app_mul {𝔸 : Type*} [DecidableEq 𝔸] [Infinite 
     (finInjCocone F).w ((finInjPermIso (π₁ * π₂)).inv.app s),
     (finInjCocone F).w ((finInjPermIso π₂).inv.app s)]
 
+/-- We define the permutation action of `Finperm 𝔸` on the colimit of a functor `FinInj 𝔸 ⥤ Type _`
+by the universal property of the colimit. This avoids dealing with any heterogeneous equalities
+or transports. -/
 instance {𝔸 : Type*} [DecidableEq 𝔸] [Infinite 𝔸] (F : FinInj 𝔸 ⥤ Type _) :
     HasPerm 𝔸 ((finInjColimit 𝔸).obj F) where
   perm π := (finInjCocone_isColimit F).desc (finInjPermCocone F π)
@@ -185,3 +187,66 @@ instance {𝔸 : Type*} [DecidableEq 𝔸] [Infinite 𝔸] (F : FinInj 𝔸 ⥤ 
     intro s
     rw [finInjPermCocone_ι_app_mul]
     rfl
+
+theorem supports_finInjLeg {𝔸 : Type*} [DecidableEq 𝔸] [Infinite 𝔸] {F : FinInj 𝔸 ⥤ Type _}
+    (s : FinInj 𝔸) (x : F.obj s) :
+    Supports s.val (finInjLeg s x) := by
+  intro π hπ
+  rw [finInjColimit_perm_def, finInjLeg,
+    ← (finInjCocone_isColimit F).uniq (finInjPermCocone F π) (𝟙 ((finInjCocone F).pt)),
+    types_id_apply]
+  intro j
+  exact ((finInjCocone F).w ((finInjPermIso π).inv.app j)).symm
+
+instance {𝔸 : Type*} [DecidableEq 𝔸] [Infinite 𝔸] (F : FinInj 𝔸 ⥤ Type _) :
+    Nominal 𝔸 ((finInjColimit 𝔸).obj F) where
+  supported x := by
+    induction x using finInjLeg_inductionOn
+    case h s x =>
+    exact ⟨_, supports_finInjLeg s x⟩
+
+theorem finInjColimit_map_equivariant' {𝔸 : Type*} [DecidableEq 𝔸] [Infinite 𝔸]
+    {F G : FinInj 𝔸 ⥤ Type _} (f : F ⟶ G) (π : Finperm 𝔸) :
+    (finInjCocone_isColimit F).desc (finInjPermCocone F π) ≫
+      (finInjCocone_isColimit F).desc ((Cocones.precompose f).obj (finInjCocone G)) =
+    (finInjCocone_isColimit F).desc ((Cocones.precompose f).obj (finInjCocone G)) ≫
+        (finInjCocone_isColimit G).desc (finInjPermCocone G π) := by
+  trans (finInjCocone_isColimit F).desc ((Cocones.precompose f).obj (finInjPermCocone G π))
+  · apply (finInjCocone_isColimit F).uniq
+      ((Cocones.precompose f).obj (finInjPermCocone G π))
+    intro s
+    have := f.naturality ((finInjPermIso π).inv.app s)
+    simp only [finInjPermCocone, const_obj_obj, Cocones.precompose_obj_pt, IsColimit.fac_assoc,
+      Category.assoc, IsColimit.fac, Cocones.precompose_obj_ι, NatTrans.comp_app]
+    simp only [id_obj, finInjPermFunctor] at this
+    rw [← Category.assoc, this]
+    rfl
+  · symm
+    apply (finInjCocone_isColimit F).uniq
+      ((Cocones.precompose f).obj (finInjPermCocone G π))
+    intro s
+    simp only [Cocones.precompose_obj_pt, const_obj_obj, IsColimit.fac_assoc,
+      Cocones.precompose_obj_ι, NatTrans.comp_app, Category.assoc, IsColimit.fac]
+
+theorem finInjColimit_map_equivariant {𝔸 : Type*} [DecidableEq 𝔸] [Infinite 𝔸]
+    {F G : FinInj 𝔸 ⥤ Type _} (f : F ⟶ G) :
+    Equivariant 𝔸 (α := (finInjColimit 𝔸).obj F → (finInjColimit 𝔸).obj G)
+      ((finInjColimit 𝔸).map f) := by
+  intro π
+  ext x
+  rw [Function.perm_def, perm_eq_iff_eq_inv_perm, finInjColimit_map, finInjColimit_perm_def,
+    finInjColimit_perm_def]
+  rw [← types_comp_apply _ ((finInjCocone_isColimit F).desc _),
+    ← types_comp_apply _ ((finInjCocone_isColimit G).desc _),
+    finInjColimit_map_equivariant']
+
+def finInjToNominal.{u, v} (𝔸 : Type u) [DecidableEq 𝔸] [Infinite 𝔸] :
+    (FinInj 𝔸 ⥤ Type (max u v)) ⥤ Bundled.{max u v} (Nominal 𝔸) where
+  obj F := ⟨(finInjColimit 𝔸).obj F, inferInstance⟩
+  map {F G} f := ⟨(finInjColimit 𝔸).map f, finInjColimit_map_equivariant f⟩
+  map_id F := by
+    apply EQ.ext
+    exact (finInjColimit 𝔸).map_id F
+  map_comp f g := by
+    apply EQ.ext
+    exact (finInjColimit 𝔸).map_comp f g
